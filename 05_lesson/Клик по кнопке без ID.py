0b84ce6719694258be2a278279152ec7e04e88f3
lenakam1975 <lee2024skypro@gmail.com>
2025-03-03 00:10:57 +0700

    HW5

---
+++ 05_lesson/Клик по кнопке без ID.py
@@ -0,0 +1,21 @@
+from time import sleep
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+
+#Кликните на синюю кнопку. Запустите скрипт три раза подряд. Убедитесь, что он отработает одинаково.
+
+driver.get('http://uitestingplayground.com/dynamicid')
+button = driver.find_element(By.XPATH, "//button[contains(text(), 'Button with Dynamic ID')]")
+button.click()
+
+
+sleep(2)
+
+
+
+
---
+++ 05_lesson/Клик по кнопке с CSS-классом.py
@@ -0,0 +1,21 @@
+from time import sleep
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+
+#Кликните на синюю кнопку. Запустите скрипт три раза подряд. Убедитесь, что он отработает одинаково.
+
+driver.get("https://uitestingplayground.com/classattr")
+button = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.CSS_SELECTOR, '.btn-primary'))
+)
+button.click()
+
+sleep(2)
+
