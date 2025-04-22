aefdb4887a7ca0a63ffa28ef95d2cf948498fc96
lenakam1975 <lee2024skypro@gmail.com>
2025-04-22 12:16:07 +0700

    HW_9

--- 09_lesson/test_subject_table.py
+++ 09_lesson/test_subject_table.py
@@ -5,38 +5,55 @@ db_connection_string = "postgresql://postgres:684000@localhost:5432/QA"
 db = create_engine(db_connection_string)
 
 
-# Добавление сущности (предмет Swimming)
+# Добавление сущности (предмет Drawing)
 def test_insert():
     connection = db.connect()
     transaction = connection.begin()
     sql = text("insert into subject (\"subject_title\") values (:new_name)")
-    connection.execute(sql, {"new_name": "Swimming"})
-
+    names = connection.execute(sql, {"new_name": "Drawing"})
     transaction.commit()
+
+    result = connection.execute(text(
+        "select subject_title from subject where subject_title = :new_name"),
+        {"new_name": "Drawing"})
+    row = result.fetchone()
+    assert row[0] == 'Drawing'
+
     connection.close()
 
 
-# Обновление сущности (добавлен ID для предмета Swimming)
+# Обновление сущности (добавлен ID для предмета Drawing)
 def test_update():
     connection = db.connect()
     transaction = connection.begin()
 
     sql = text(
-        "update subject set subject_id = :id where subject_title = :new_name"
+        "update subject set subject_id = :new_id where subject_id is NULL and subject_title = :title"
     )
-    connection.execute(sql, {"id": 16, "new_name": 'Swimming'})
-
+    connection.execute(sql, {"new_id": 16, "title": "Drawing"})
     transaction.commit()
+
+    result = connection.execute(text(
+        "select subject_id from subject where subject_title = :title"),
+        {"title": "Drawing"})
+    row = result.fetchone()
+    assert row[0] == 16
+
     connection.close()
 
 
-# Удаление сущности (удален предмет Swimming)
+# Удаление сущности (удален предмет Drawing)
 def test_delete():
     connection = db.connect()
     transaction = connection.begin()
 
     sql = text("delete from subject where subject_id = :id")
     connection.execute(sql, {"id": 16})
-
     transaction.commit()
+
+    result = connection.execute(text(
+        "select * from subject where subject_id = :id"), {"id": 16})
+    row = result.fetchone()
+    assert row is None
+
     connection.close()
